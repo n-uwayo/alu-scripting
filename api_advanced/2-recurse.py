ee09eb4e86995184d3a6adc411410cce1a612a9f
n-uwayo <122350054+n-uwayo@users.noreply.github.com>
2023-07-14 10:54:25 +0000

    api

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,32 +1,31 @@
 #!/usr/bin/python3
-""""Doc"""
+"""Contains recurse function"""
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=""):
-    """"Doc
-    Reddit sends an after property in the response.
-    Keep retrieving comments until after is null.
-    """
-    url = "https://www.reddit.com/r/{}/hot.json" \
-        .format(subreddit)
-    header = {'User-Agent': 'Mozilla/5.0'}
-    param = {'after': after}
-    res = requests.get(url, headers=header, params=param)
-
-    if res.status_code != 200:
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """Returns a list of titles of all hot posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
         return None
-    else:
-        json_res = res.json()
-        # print(json_res.get('data').get('after'))
-        after = json_res.get('data').get('after')
-        has_next = \
-            json_res.get('data').get('after') is not None
-        # print(has_next)
-        hot_articles = json_res.get('data').get('children')
-        [hot_list.append(article.get('data').get('title'))
-         for article in hot_articles]
-        # print(len(hot_list))
-        # print(hot_list)
-        return recurse(subreddit, hot_list, after=after) \
-            if has_next else hot_list
+
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
