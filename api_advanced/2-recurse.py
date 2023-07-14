e1a0fc287735457dc5e46841857d2e9bb4b974db
n-uwayo <122350054+n-uwayo@users.noreply.github.com>
2023-07-14 11:11:18 +0000

    advanced

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -2,17 +2,14 @@
 """
 Recursive function that queries the Reddit API and returns
 a list containing the titles of 
-all hot articles for a given subreddit.
-If no results are found for the given subreddit,
-the function should return None"""
+all hot articles for a given subreddit."""
 import requests
 
 
 def recurse(subreddit, hot_list=[], after="", count=0):
    """
     Queries the Reddit API and returns
-    a list containing the titles of all 
-    hot articles for a given subreddit."""
+    a list containing the titles ."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "0x16-api_advanced:project:\
