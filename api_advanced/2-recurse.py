23a33d73ee07c117f5cc5133a6e16dc45a80d897
n-uwayo <122350054+n-uwayo@users.noreply.github.com>
2023-07-14 11:16:50 +0000

    api

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Contains recurse function"""
+"""recurse function"""
 import requests
 
 
 def recurse(subreddit, hot_list=[], after="", count=0):
-    """Returns a list of titles of all hot posts on a given subreddit."""
+    """Returns a list of titles."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "0x16-api_advanced:project:\
