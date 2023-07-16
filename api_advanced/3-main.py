dcee78ab111f6b04c90779f1373fbb8bfeb83a61
n-uwayo <122350054+n-uwayo@users.noreply.github.com>
2023-07-16 20:42:05 +0000

    api

--- api_advanced/3-main.py
+++ api_advanced/3-main.py
@@ -1,13 +1,11 @@
-# # #!/usr/bin/python3
-# """
-# 100-main
-# """
-# import sys
+# #!/usr/bin/python3
 
-# if __name__ == '__main__':
-#     count_words = __import__('100-count').count_words
-#     if len(sys.argv) < 3:
-#         print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
-#         print("Ex: {} programming 'python java javascript'".format(sys.argv[0]))
-#     else:
-#         result = count_words(sys.argv[1], [x for x in sys.argv[2].split()])
+import sys
+
+if __name__ == '__main__':
+    count_words = __import__('3-count').count_words
+    if len(sys.argv) < 3:
+        print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
+        print("Ex: {} programming 'python java javascript'".format(sys.argv[0]))
+    else:
+        result = count_words(sys.argv[1], [x for x in sys.argv[2].split()])
